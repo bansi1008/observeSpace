7c19909e406a1f9b822c8b71374d92b5d0c5abb3
Bansi Dobariya <148076698+bansi1008@users.noreply.github.com>
2025-07-27 12:23:11 +0100

    Update NDWIChangeDetection.js

--- src/app/components/NDWIChangeDetection.js
+++ src/app/components/NDWIChangeDetection.js
@@ -18,7 +18,7 @@ const mapCenter = {
   lng: -114.7377, // Lake Mead, Nevada
 };
 
-const instanceId = "e6f303d3-5f1b-4ea7-912e-6f86c0df3666";
+const instanceId = "bfcaa3bf-5335-4f2e-b704-68a535704a9e";
 
 // Predefined water regions of interest
 const presetRegions = [
